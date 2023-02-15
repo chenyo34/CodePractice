b82d3178cae3c40a0fac4ac18d396eef651cc576
chenyo34 <2277208307@qq.com>
2023-02-15 00:53:14 -0500

    Update Tree.py
    Correct the algorithm to find the path

--- Tiktok/Tree.py
+++ Tiktok/Tree.py
@@ -9,29 +9,33 @@ class Tree:
         self.t_to = t_to
         self.val = val
 
-    def getDistance(self, x, y):
+    def getPath(self, x, y, path = []):
 
         # If the given nodes are the same
         if x == y:
-            return 0
+            return [x]
 
-        children = []
+        negbs = []
         for i in range(self.t - 1):
-            if self.t_from[i] == x:
-                children.append(self.t_to[i])
-            if self.t_to[i] == x:
-                children.append(self.t_from[i])
-        dist = self.t
-        print(children)
-        for child in children:
-            dist = min(self.getDistance(child, y), dist)
+            if self.t_from[i] == x and self.t_to[i] not in path:
+                negbs.append(self.t_to[i])
+            if self.t_to[i] == x and self.t_from[i] not in path:
+                negbs.append(self.t_from[i])
 
-        return dist + 1
+        path.append(x)
+
+        if negbs == []:
+            return [-1]
+
+        for negb in negbs:
+            print(negb)
+            curPath = self.getPath(negb, y, path)
+            print(path.append(curPath))
 
 
-my_tree = Tree(6,
+my_tree = Tree(7,
                [1, 1, 2, 2, 3, 6],
                [2, 3, 4, 5, 6, 7],
                [1, 2, 3, 4, 5, 3, 2])
-d1 = my_tree.getDistance(1, 7)
+d1 = my_tree.getPath(2, 7)
 print(d1)
